46a36c29fbec6dbf67f3cdf531b45fed7744e171
hoaibao44 <hoaibao.nhb@gmail.com>
2022-09-09 12:49:53 +0900

    btn ok

--- src/app/component/cs-button/cs-button.component.ts
+++ src/app/component/cs-button/cs-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cs-button',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cs-button.component.scss']
 })
 export class CsButtonComponent implements OnInit {
-
+  @Input() textString:string = 'Button'; 
+  @Input() btnStyle:string = 'primary'; 
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.textString)
   }
 
 }
